9fddbf915d4d7f2c087212bd52100ed6cd7e7bb5
trisrosco <trisrosco@gmail.com>
2023-04-18 16:12:04 +0100

    IT WORKS HAHAHAHA

--- cyoa/src/App.js
+++ cyoa/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Button from "./components/Button/index.js";
 import NarrativeBlock from "./components/NarrativeBlock/index.js";
@@ -8,25 +8,27 @@ import narrativeData from "./components/NarrativeData/index.js";
 // function to change the narrative text when a button is clicked
 
 function App() {
-  const [narrative, setNarrative] = useState(1);
+  const [currentNarrative, setCurrentNarrative] = useState(1);
+  const [button1, setButton1] = useState(
+    narrativeData[currentNarrative].options[0].text
+  );
+  const [button2, setButton2] = useState(
+    narrativeData[currentNarrative].options[1].text
+  );
 
-  const HandleNarrative = (event) => {
-    setNarrative(event.target.value);
-  };
+  function changeNarrative() {
+    setCurrentNarrative(
+      narrativeData[currentNarrative].options[0].nextNarrative
+    );
+    setButton1(narrativeData[currentNarrative].options[0].text);
+    setButton2(narrativeData[currentNarrative].options[1].text);
+  }
 
   return (
     <div className="App">
-      <NarrativeBlock text={narrativeData[1].text} />
-      <Button
-        label={narrativeData[1].options[0].text}
-        onClick={HandleNarrative}
-        value={narrativeData[1].options[0].nextNarrative}
-      />
-      <Button
-        label={narrativeData[1].options[1].text}
-        onClick={HandleNarrative}
-        value={narrativeData[1].options[1].nextNarrative}
-      />
+      <NarrativeBlock text={narrativeData[currentNarrative].text} />
+      <Button label={button1} onClick={changeNarrative} />
+      <Button label={button2} onClick={changeNarrative} />
     </div>
   );
 }
